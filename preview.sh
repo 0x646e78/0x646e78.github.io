7bf9bd018148851211724ba0e2772c0f82ff4d49
DNX <auraltension@riseup.net>
2022-03-16 17:05:44 +1100

    try classic theme

--- preview.sh
+++ preview.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Will be available on http://localhost:3000"
+docker run --rm --volume="$PWD:/srv/jekyll" -p 3000:4000 jekyll/jekyll:latest bundle update
 docker run --rm --volume="$PWD:/srv/jekyll" -p 3000:4000 jekyll/jekyll:latest jekyll serve --drafts
